6529a2988b99a6b058ed90d53c78604c760eced2
Pablo Bacho <pablo@fabricadigital.org>
2019-05-31 12:03:40 +0200

    Add usage info

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # m5stickc-idf-example
-Example project for the M5StickC-IDF component
+
+Example project for the M5StickC-IDF component. Works without including the Arduino framework.
+
+See the ESP-IDF component and features here: [M5StickC-IDF](https://github.com/pablobacho/M5StickC-IDF)
+
+---
+
+## Usage
+
+This repository includes the M5StickC-IDF component as a submodule. 
+
+You can clone it using:
+
+```
+git clone --recurse-submodules https://github.com/pablobacho/m5stickc-idf-example
+```
+
+Or, if you already cloned it:
+
+```
+git submodule init
+git submodule update
+```
+
+Your WiFi SSID and password can be set using menuconfig.
+
+## Contributing 
+
+If you find it useful and add a feature, fix a bug, improve documentation or anything else, your pull requests are more than welcome. Please make sure your code follows the ESP-IDF style.
+
+This is a work in progress. It will be updated with new features as they are needed, and we could definitely use your help!
+
+Licensed under the MIT License.
---
+++ main/main.c
@@ -0,0 +1,54 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+
+#include "M5StickC.h"
+
+static const char *TAG = "MAIN";
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK;
+}
+
+void app_main(void)
+{
+    nvs_flash_init();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .password = CONFIG_ESP_WIFI_PASSWORD,
+            .bssid_set = false
+        }
+    };
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+
+    m5stickc_init();
+
+    font_rotate = 0;
+    text_wrap = 0;
+    font_transparent = 0;
+    font_forceFixed = 0;
+    gray_scale = 0;
+    TFT_setGammaCurve(DEFAULT_GAMMA_CURVE);
+    TFT_setRotation(LANDSCAPE);
+    TFT_setFont(DEFAULT_FONT, NULL);
+    TFT_resetclipwin();
+    TFT_fillScreen(TFT_WHITE);
+    _bg = TFT_WHITE;
+    _fg = TFT_MAGENTA;
+    TFT_print("Test", CENTER, (DISPLAY_HEIGHT-24)/2);
+}
+
